7f67820e9456175ce8d9fb62a9386dd90b43aebb
Sergey Razdolyansky <grafcorp01@gmail.com>
2018-09-11 23:46:05 +0300

    Add empty project

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 'use strict';
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const del = require('del');
 
-
-gulp.task('default', ['clean'], function() {
-    // gulp.start('sass', 'browser-sync', 'sprite','fileinclude','watch');
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./src/sccs/**/*.scss')
+    return gulp.src('./src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./build/css'));
 });
+
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+
+});
+
+gulp.task('clean', function() {
+    return del(['build/css']);
+});
+
+gulp.task('default', gulp.series(
+    'sass', 'browser-sync','watch'
+));
